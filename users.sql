49c4e22a810722f6a1fcde521d50d1188fd36801
Alex <shamshin.alexandr@gmail.com>
2014-04-03 14:54:17 +0300

    dump for base

---
+++ users.sql
@@ -0,0 +1,274 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Апр 03 2014 г., 14:53
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `authassignment`
+--
+
+CREATE TABLE IF NOT EXISTS `authassignment` (
+  `itemname` varchar(64) NOT NULL,
+  `userid` varchar(64) NOT NULL,
+  `bizrule` text,
+  `data` text,
+  PRIMARY KEY (`itemname`,`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `authassignment`
+--
+
+INSERT INTO `authassignment` (`itemname`, `userid`, `bizrule`, `data`) VALUES
+('Admin', '1', NULL, 'N;'),
+('Authenticated', '2', NULL, 'N;'),
+('Moderator', '3', NULL, 'N;');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `authitem`
+--
+
+CREATE TABLE IF NOT EXISTS `authitem` (
+  `name` varchar(64) NOT NULL,
+  `type` int(11) NOT NULL,
+  `description` text,
+  `bizrule` text,
+  `data` text,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `authitem`
+--
+
+INSERT INTO `authitem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES
+('Admin', 2, NULL, NULL, 'N;'),
+('Authenticated', 2, NULL, NULL, 'N;'),
+('Guest', 2, NULL, NULL, 'N;'),
+('Moderator', 2, 'Модератор', NULL, 'N;'),
+('Tovar.Create', 0, NULL, NULL, 'N;'),
+('Tovar.Delete', 0, NULL, NULL, 'N;'),
+('Tovar.Index', 0, NULL, NULL, 'N;'),
+('Tovar.Update', 0, NULL, NULL, 'N;'),
+('Tovar.View', 0, NULL, NULL, 'N;'),
+('updateOwnTovar', 1, 'Создание и удаление товара', ' return Yii::app()->user->id == Post::model()->findByPk($_GET[''id''])->Id;', 'N;');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `authitemchild`
+--
+
+CREATE TABLE IF NOT EXISTS `authitemchild` (
+  `parent` varchar(64) NOT NULL,
+  `child` varchar(64) NOT NULL,
+  PRIMARY KEY (`parent`,`child`),
+  KEY `child` (`child`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `authitemchild`
+--
+
+INSERT INTO `authitemchild` (`parent`, `child`) VALUES
+('Authenticated', 'Guest'),
+('Moderator', 'Guest'),
+('Authenticated', 'Tovar.Create'),
+('Moderator', 'Tovar.Delete'),
+('Guest', 'Tovar.Index'),
+('Moderator', 'Tovar.Update'),
+('updateOwnTovar', 'Tovar.Update'),
+('Guest', 'Tovar.View'),
+('Authenticated', 'updateOwnTovar');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `rights`
+--
+
+CREATE TABLE IF NOT EXISTS `rights` (
+  `itemname` varchar(64) NOT NULL,
+  `type` int(11) NOT NULL,
+  `weight` int(11) NOT NULL,
+  PRIMARY KEY (`itemname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tbl_profiles`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_profiles` (
+  `user_id` int(11) NOT NULL,
+  `lastname` varchar(50) NOT NULL DEFAULT '',
+  `firstname` varchar(50) NOT NULL DEFAULT '',
+  `birthday` date NOT NULL DEFAULT '0000-00-00',
+  `filename` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `tbl_profiles`
+--
+
+INSERT INTO `tbl_profiles` (`user_id`, `lastname`, `firstname`, `birthday`, `filename`) VALUES
+(1, 'Admin', 'Administrator', '0000-00-00', ''),
+(2, 'Demo', 'Demo', '0000-00-00', ''),
+(3, 'shamshin', 'alex', '1985-09-11', ''),
+(4, 'shamshin', 'alex', '0000-00-00', '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tbl_profiles_fields`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_profiles_fields` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `varname` varchar(50) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `field_type` varchar(50) NOT NULL,
+  `field_size` int(3) NOT NULL DEFAULT '0',
+  `field_size_min` int(3) NOT NULL DEFAULT '0',
+  `required` int(1) NOT NULL DEFAULT '0',
+  `match` varchar(255) NOT NULL DEFAULT '',
+  `range` varchar(255) NOT NULL DEFAULT '',
+  `error_message` varchar(255) NOT NULL DEFAULT '',
+  `other_validator` varchar(5000) NOT NULL DEFAULT '',
+  `default` varchar(255) NOT NULL DEFAULT '',
+  `widget` varchar(255) NOT NULL DEFAULT '',
+  `widgetparams` varchar(5000) NOT NULL DEFAULT '',
+  `position` int(3) NOT NULL DEFAULT '0',
+  `visible` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `varname` (`varname`,`widget`,`visible`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Дамп данных таблицы `tbl_profiles_fields`
+--
+
+INSERT INTO `tbl_profiles_fields` (`id`, `varname`, `title`, `field_type`, `field_size`, `field_size_min`, `required`, `match`, `range`, `error_message`, `other_validator`, `default`, `widget`, `widgetparams`, `position`, `visible`) VALUES
+(1, 'lastname', 'Last Name', 'VARCHAR', 50, 3, 1, '', '', 'Incorrect Last Name (length between 3 and 50 characters).', '', '', '', '', 1, 3),
+(2, 'firstname', 'First Name', 'VARCHAR', 50, 3, 1, '', '', 'Incorrect First Name (length between 3 and 50 characters).', '', '', '', '', 0, 3),
+(3, 'birthday', 'Birthday', 'DATE', 0, 0, 2, '', '', '', '', '0000-00-00', 'UWjuidate', '{"ui-theme":"redmond"}', 3, 2),
+(4, 'filename', 'Аватар', 'VARCHAR', 255, 0, 2, '', '', '', '', '', '', '', 0, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tbl_tovar`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_tovar` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `price` int(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+
+--
+-- Дамп данных таблицы `tbl_tovar`
+--
+
+INSERT INTO `tbl_tovar` (`id`, `name`, `price`) VALUES
+(2, 'Детские игрушки', 340),
+(3, 'Взрослые игрушки6', 444),
+(4, 'тестовій товар', 33),
+(5, 'game1', 22),
+(6, 'tovar by tester3', 43),
+(7, 'tov1', 5),
+(8, 'tov2', 33),
+(9, 'tov2', 33),
+(10, 'tov3', 12),
+(11, 'tov4', 123),
+(12, 'tov5', 21),
+(13, 'tov6', 4),
+(14, 'tov7', 32),
+(15, 'tov8', 45),
+(16, 'tov9', 22),
+(17, 'tov10', 344),
+(18, 'tesss', 687);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tbl_users`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `activkey` varchar(128) NOT NULL DEFAULT '',
+  `createtime` int(10) NOT NULL DEFAULT '0',
+  `lastvisit` int(10) NOT NULL DEFAULT '0',
+  `superuser` int(1) NOT NULL DEFAULT '0',
+  `status` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  KEY `status` (`status`),
+  KEY `superuser` (`superuser`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Дамп данных таблицы `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`id`, `username`, `password`, `email`, `activkey`, `createtime`, `lastvisit`, `superuser`, `status`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'webmaster@example.com', '9a24eff8c15a6a141ece27eb6947da0f', 1261146094, 1396525149, 1, 1),
+(2, 'demo', 'fe01ce2a7fbac8fafaed7c982a04e229', 'demo@example.com', '099f825543f7850cc038b90aaff39fac', 1261146096, 0, 0, 1),
+(3, 'moderator', '3d801aa532c1cec3ee82d87a99fdf63f', 'shamshin@i.ua', '3b81f1cee79f9c280a7acef2810918c5', 1396161609, 1396166012, 1, 1),
+(4, 'tester', '098f6bcd4621d373cade4e832627b4f6', 'shamshin1@i.ua', 'b56cfb346a0011ff62937151c268af0d', 1396162638, 1396523548, 0, 1);
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `authassignment`
+--
+ALTER TABLE `authassignment`
+  ADD CONSTRAINT `authassignment_ibfk_1` FOREIGN KEY (`itemname`) REFERENCES `authitem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `authitemchild`
+--
+ALTER TABLE `authitemchild`
+  ADD CONSTRAINT `authitemchild_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `authitem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `authitemchild_ibfk_2` FOREIGN KEY (`child`) REFERENCES `authitem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `rights`
+--
+ALTER TABLE `rights`
+  ADD CONSTRAINT `rights_ibfk_1` FOREIGN KEY (`itemname`) REFERENCES `authitem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
